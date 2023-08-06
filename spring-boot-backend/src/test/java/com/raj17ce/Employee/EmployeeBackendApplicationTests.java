d8558cf45a0e82735de82226c3672ddcbed6f6b2
Raj Patel <raj17ce@gmail.com>
2023-08-06 09:35:17 +0530

    Created folder structure

---
+++ spring-boot-backend/src/test/java/com/raj17ce/Employee/EmployeeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj17ce.Employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
